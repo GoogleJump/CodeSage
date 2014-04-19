e8b16d68b1be625caf838fea0782977543a3abb6
Devon Bernard <dwbcoding@gmail.com>
2014-04-19 02:41:51 -0400

    Changed project from PHP-based to Python running the Flask framework.

--- php/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function getDBH() {
-		$dbUSER = "Username";
-		$dbPASS = "Password";
-		$dbh = new PDO('mysql:host=localhost;dbname=myDatabaseName', $dbUSER, $dbPASS);
-		return $dbh;
-	}
-?>
