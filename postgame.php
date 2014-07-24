d39e9e93aa4ba6d654e7ea910815c7202d587a75
Reza Nayebi <rn2324@columbia.edu>
2014-07-25 01:06:31 +0200

    Changed design of play page, added timer and facebook picture and selection of language and number of games for first page

---
+++ postgame.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<title>CodeWars</title>
+    	<link href="assets/codemirror-4.0/lib/codemirror.css" rel="stylesheet">
+    	<link href="assets/bootstrap-3.1.1-dist/css/bootstrap.min.css" rel="stylesheet">
+    	<link href="playpage.css" rel="stylesheet">
+    	<script src="assets/jquery-1.11.1.min.js"></script>
+    	<script src="assets/codemirror-4.0/lib/codemirror.js"></script>
+		<script src="assets/codemirror-4.0/mode/python/python.js"></script>
+		<script src="assets/codemirror-4.0/mode/javascript/javascript.js"></script>
+		<script src="assets/codemirror-4.0/mode/clike/clike.js"></script>
+		<script type="text/javascript" src="playpage.js"></script>	
+    </head>
+<body>
+Test
+</body>
+</html>
