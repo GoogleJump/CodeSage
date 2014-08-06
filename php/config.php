8b51def70a7ca6eac6e608596326a59e50879f5e
Devon Bernard <dwbcoding@gmail.com>
2014-08-06 13:48:54 -0400

    Revert "Final Project Demo"
    This reverts commit 0af454b9856c96669c5cea8dfafdfae143799c75.

--- php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	function getDBH() {
-		$dbID = "***********";
-		$dbPASS = "**********";
-		$dbh = new PDO('mysql:host=localhost;dbname=******', $dbID, $dbPASS);
-		return $dbh;
-	}
-?>
